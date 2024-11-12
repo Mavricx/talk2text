0632d7939ca34f07bec5c2af09fe3dc11883e857
Priyansu Pattanayak <ppattanayakcodes@gmail.com>
2024-11-13 05:18:53 +0530

    changed language from hindi to english

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 let element = document.querySelector(".text");
 const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
-recognition.lang = "hi-IN";
+recognition.lang = "en-IN";
 recognition.continuous = true;
 const mic = document.querySelector(".mp");
 const stop = document.querySelector(".stop");
 const copy = document.querySelector(".copy");
 
 copy.addEventListener("click", () => {
-   
-    navigator.clipboard.writeText(element.value).then(()=>{
+
+    navigator.clipboard.writeText(element.value).then(() => {
         alert("Text copied to clipboard");
-    }).catch((error)=>{
+    }).catch((error) => {
         console.log("copy failed: " + error)
     })
 });
